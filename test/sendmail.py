860a48eb29de372d4eca8f2c1955c534b6568fd0
whsasf <whsasf@126.com>
2018-03-05 23:15:19 +0800

    python

--- test/sendmail.py
+++ test/sendmail.py
@@ -1,32 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
- 
+#!/usr/bin/python3
+
 import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
- 
-# 第三方 SMTP 服务
-mail_host="10.49.58.239"  #设置服务器
-mail_user="XXXX"    #用户名
-mail_pass="XXXXXX"   #口令 
- 
- 
-sender = 'xx1@openwave.com'
-receivers = ['xx2@openwave.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
- 
-message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-message['From'] = Header("菜鸟教程", 'utf-8')
-message['To'] =  Header("测试", 'utf-8')
- 
-subject = 'Python SMTP 邮件测试'
-message['Subject'] = Header(subject, 'utf-8')
- 
- 
+
+sender = 'xx2@openwave.com'
+receivers = ['xx1+Trash@openwave.com']
+
+message = """From: From Person <xx2@openwave.com>
+To: To Person <xx1+Trash@openwave.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+
 try:
-    smtpObj = smtplib.SMTP() 
-    smtpObj.connect(mail_host, 20025)    # 20025 为 SMTP 端口号
-    smtpObj.login(mail_user,mail_pass)  
-    smtpObj.sendmail(sender, receivers, message.as_string())
-    print ("邮件发送成功")
-except smtplib.SMTPException:
-    print ("Error: 无法发送邮件")
+   smtpObj = smtplib.SMTP('10.49.58.239',20025)
+   smtpObj.sendmail(sender, receivers, message)         
+   print ("Successfully sent email")
+except SMTPException:
+   print ("Error: unable to send email")
