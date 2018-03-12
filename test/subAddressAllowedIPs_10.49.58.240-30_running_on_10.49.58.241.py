65475bcbd98c9d0b83029ca020a40adbf42c61e2
whsasf <whsasf@126.com>
2018-03-12 13:40:19 +0800

    new

---
+++ test/subAddressAllowedIPs_10.49.58.240-30_running_on_10.49.58.241.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+from modules.sendmails import send_mail
+from modules.remote_operations import remote_operation
+
+#reate summary file and debug files:
+#sumfile = 'sumary.log'
+#dbgfile = 'debug.log'
+#with open(sumfile, 'a') as file_object:
+#    file_object.write(attdata)
+
+mtahost = '10.49.58.239'
+mtaport = 20025
+sshtarget = '10.49.58.239'
+sshaccount = 'root'
+sshpasswd = 'letmein'
+sender = 'u2 <u2@openwave.com>'
+
+#define all the variables needed
+testcases = {
+'t1':{'casename':'MX-11058','receivers':['u1 <u1+INBOX@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to INBOX'},
+'t2':{'casename':'MX-11059','receivers':['u1 <u1+Trash@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to Trash'},
+'t3':{'casename':'MX-11060','receivers':['u1 <u1+Trash/folder@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to Trash/folder'},
+'t4':{'casename':'MX-11061','receivers':['u1 <u1+Trash/folder/folder1/folder2/folder3@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to Trash/folder/folder1/folder2/folder3'},
+'t5':{'casename':'MX-11062','receivers':['u1 <INBOX--u1@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to INBOX'},
+'t6':{'casename':'MX-11063','receivers':['u1 <Trash--u1@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to Trash'},
+'t7':{'casename':'MX-11064','receivers':['u1 <Trash/folder--u1@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to Trash/folder'},
+'t8':{'casename':'MX-11065','receivers':['u1 <Trash/folder/folder1/folder2/folder3--u1@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to Trash/folder/folder1/folder2/folder3'},
+'t9':{'casename':'MX-11066','receivers':['u1 <u1+test@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to test'},
+'t10':{'casename':'MX-11067','receivers':['u1 <u1+test/folder@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to test/folder'},
+'t11':{'casename':'MX-11068','receivers':['u1 <u1+test/folder/folder1/folder2/folder3@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to test/folder/folder1/folder2/folder3'},
+'t12':{'casename':'MX-11068','receivers':['u1 <test--u1@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to test'},
+'t13':{'casename':'MX-11070','receivers':['u1 <test/folder--u1@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to test/folder'},
+'t14':{'casename':'MX-11071','receivers':['u1 <test/folder/folder1/folder2/folder3--u1@openwave.com>'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to test/folder/folder1/folder2/folder3'},
+            }
+#set  subAddressAllowedIPs=127.0.0.1
+remote_operation('su - imail -c "imconfcontrol -install -key \"/*/mta/subAddressAllowedIPs=10.49.58.240/30\";imconfcontrol -install -key \"/site1-inbound-standardmta-direct/mta/subAddressAllowedIPs=10.49.58.240/30\""','10.49.58.239','root','letmein',0)
+
+# restart mta server
+remote_operation('su - imail -c "~/lib/imservctrl killStart mta"', '10.49.58.239','root','letmein',1,'imservctrl: done',1)
+
+print ('Delete u1,u2,u3 if already existed...',end='') #delete u1,u2,u3 if exists
+remote_operation('su - imail -c \
+  "account-delete u1@openwave.com;account-delete u2@openwave.com;account-delete u3@openwave.com"',\
+  '10.49.58.239','root','letmein',1,'Mailbox Deleted Successfully',3)
+    
+print ('Create u1,u2,u3 ...                  ',end='') #creaet account u1,u2,u3
+remote_operation('su - imail -c \
+  "account-create u1@openwave.com p default;account-create u2@openwave.com p default;account-create u3@openwave.com p default"',\
+  '10.49.58.239','root','letmein',1,'MailboxId',3)
+
+print ('Clear mta.log firsltly ...           ',end='') #clear mta.log firstly
+remote_operation('su - imail -c "> log/mta.log"','10.49.58.239','root','letmein',0)
+
+#set quote for sender and recepients
+#ssdsdsd
+
+print ('###############Beginning testing...######################')
+for tck ,tcv in sorted(testcases.items(),key=lambda testcases:testcases[0]):
+    print ('\033[1;45mRunning testing testcase: '+tck+'---------->'+tcv['casename']+'\033[0m')
+    sendnum = len(tcv['receivers'])
+    #print ('recivers numbers are:'+str(sendnum))
+    print ('Sending message in proper formats ...',end='')
+    send_mail(mtahost,mtaport,sender,tcv['receivers'])
+    print ('Checking mta.log ...                 ',end='')
+    remote_operation(tcv['commands'],sshtarget,sshaccount,sshpasswd,1,tcv['check_flags'],sendnum)
+print ('###############Endding testing...######################') 
+
+print ('Delete u1,u2,u3 ...',end='') #delete u1,u2,u3 at last
+remote_operation('su - imail -c \
+  "account-delete u1@openwave.com;account-delete u2@openwave.com;account-delete u3@openwave.com"',\
+  '10.49.58.239','root','letmein',1,'Mailbox Deleted Successfully',3)
