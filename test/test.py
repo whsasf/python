dc0e09f9ee3c85837f670340cbd9f24bd8f478af
whsasf <whsasf@126.com>
2018-03-19 15:38:15 +0800

    new

--- test/test.py
+++ test/test.py
@@ -18,7 +18,7 @@ sender = 'u2 <u2@openwave.com>'
 
 #define all the variables needed
 testcases = {
-'t24':{'casename':'MX-11221','receivers':['"u1+DDD\"LL \\@openwave.com"'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':"delivered to DDD"LL \"},
+'t24':{'casename':'MX-11221','receivers':['"u1+DDD\"LL \\@openwave.com"'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':"delivered to DDD\"LL \\"},
 't31':{'casename':'MX-11228','receivers':['"te st---u1@openwave.com"'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to te st-'},
 't32':{'casename':'MX-11229','receivers':['"te\"st---u1@openwave.com"'],'commands':'su - imail -c "cat log/mta.log;> log/mta.log"','check_flags':'delivered to te"st-'},
            }
