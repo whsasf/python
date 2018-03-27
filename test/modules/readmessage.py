81ae1569b1430ad44701b68a7e526e3a0c7f7249
whsasf <whsasf@126.com>
2018-03-27 23:38:07 +0800

    new

--- test/modules/readmessage.py
+++ test/modules/readmessage.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python3
-
+def imap_fetch():
 from imaplib import IMAP4
 
 conn = IMAP4(host='10.49.58.239', port=20143)
 conn.login('xx1','p')
 conn.select(mailbox='INBOX', readonly=False)
-message="FROM: xx2\r\n\
+message=b"FROM: xx2\r\n\
 to:xx1\r\n\
-data\t\n\
 \r\n\
 helo world\r\n"
 
-conn.append('INBOX','unseen',1522146813,message.encode("utf-8") )
+conn.append('INBOX','unseen',1522146813,message )
 
-conn.fetch("1001",'full')
+outcome, data1=conn.fetch("1:*",'rfc822')
 
+i = 1
+#print (data1)
+#print (data)
+#print ("++++++++++++++++++++++++++++")
+for x in data1:
+    if b'FLAGS' in x or  x == b')':
+        continue
+    else:
+        print ('\033[1;32mMessage' + str(i) +": \033[0m")
+        #print (x[0].decode('utf-8'))
+        #print (x[1])
+        print (x[1].decode('utf-8'))
+        #print ("\n")
+    i=i+1
+
+outcome, data2=conn.fetch("1:*",'firstline')
+#print (data2)
+print ("\033[1;32mFirstline data: \033[0m")
+for x in data2:
+    #print (x[0].decode('utf-8'))
+    print (x.decode('utf-8'))
+    #print ("\n")
+    
 conn.logout()
