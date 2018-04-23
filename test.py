15f1bc968f2275c7e308870fb8d2511a93f745e2
whsasf <whsasf@126.com>
2018-04-23 18:05:47 +0800

    new

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+dict = {4:'wewewe',1:'3wewwer',2:'erwerwewe',3:'343rere'}
+
+print(list(map(lambda x: dict[x],sorted(dict.keys()))))
+	
+	
+list1=[0,8,1,6,2,9,4]
+print(list(map(lambda x: x ,list1)))
