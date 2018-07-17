f4e31b11c3e4e3cf4502848c4b0f2f6de781ae61
whsasf <whsasf@126.com>
2018-07-17 13:26:52 +0800

    new

--- SMTP_Subaddress/test_cases-E2E/2domains_on_same_host/1-subAddressAllowedIPs_localhost_running_on_10.49.58.147_2domains_same_server.py
+++ SMTP_Subaddress/test_cases-E2E/2domains_on_same_host/1-subAddressAllowedIPs_localhost_running_on_10.49.58.147_2domains_same_server.py
@@ -101,7 +101,7 @@ for tck ,tcv in sorted(testcases.items(),key=lambda testcases:testcases[0]):
     #print ('recivers numbers are:'+str(sendnum))
     print ('Sending message in proper formats ...',end='')
     send_mail(mtahost,mtaport,sender,tcv['receivers'])
-    time.sleep(8)
+    time.sleep(5)
     print ('Checking mta.log ...                 ',end='')
     remote_operation(tcv['commands'],sshtarget,sshaccount,sshpasswd,1,tcv['check_flags'],sendnum)
 print ('###############Endding testing...######################') 
